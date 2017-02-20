17091b033b50a87bb8986a95aba9c4a67a9f8011
RumenKotsew <kotsewrumen@gmail.com>
2017-02-20 18:14:56 +0200

    update base settings

--- project/shano_sound/shano_sound/urls.py
+++ project/shano_sound/shano_sound/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from user_connections import views as user_connections_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('user_connections.urls')),
+    url(r'', include('user_management.urls')),
+    url(r'thanks', user_connections_views.thanks),
 ]
